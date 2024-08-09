033db8f92b75f07801f82b955f93b47bcaa4f148
Hatem Ben Echikh <hatembenechikh100@gmail.com>
2024-08-09 10:29:31 +0100

    some changes

--- field.js
+++ field.js
@@ -3,27 +3,6 @@ let position = 0;
 let step = 1;
 let winner = null;
 
-const Player1Click = () => {
-  if (players.player1 !== null) {
-    position += step;
-  }
-};
-
-const Player2Click = () => {
-  if (players.player2 !== null) {
-    position += step;
-  }
-};
-
-const VerifyWinner = () => {
-  if (position > 100) {
-    winner = players.player1;
-  }
-  if (position < -100) {
-    winner = players.player2;
-  }
-};
-
 const EmmitPosition = (io) => {
   io.to(players.player1).emit("position", position);
   io.to(players.player2).emit("position", position);
@@ -54,7 +33,19 @@ const PlayerJoined = (io, socket) => {
     players.player2 = socket.id;
     socket.emit("player", 2);
   } else {
-    socket.emit("player", -1);
+    players.player1 = players.player2;
+    players.player2 = socket.id;
+    io.to(players.player1).emit("player", 1);
+    io.to(players.player2).emit("player", 2);
+  }
+
+  if (players.player1 !== null && players.player2 !== null) {
+    position = 0;
+    winner = null;
+    io.to(players.player1).emit("position", position);
+    io.to(players.player2).emit("position", position);
+    io.to(players.player1).emit("start", true);
+    io.to(players.player2).emit("start", true);
   }
 };
 
